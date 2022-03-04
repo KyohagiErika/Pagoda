4c1a13cb5e990c3c69784910390e62f467f0000c
Lê Thành Long <48479168+KyohagiErika@users.noreply.github.com>
2022-03-04 08:51:39 +0700

    Delete src directory

--- src/com/viewmodel/application/PagodaValidater.java
+++
@@ -1,8 +0,0 @@
-package com.viewmodel.application;
-
-import com.processor.applicaion.Validater;
-
-public class PagodaValidater extends Validater {
-    public static final String USERNAME_PATTERN = "";
-    public static final String PASSWORD_PATTERN = "";
-}
