1462385b7a6d5f1e44e693dcdf6564b112254dc0
Lê Thành Long <48479168+KyohagiErika@users.noreply.github.com>
2022-03-13 14:25:15 +0700

    Delete com directory

--- com/viewmodel/exception/PagodaAppException.java
+++
@@ -1,9 +0,0 @@
-package com.viewmodel.exception;
-
-public class PagodaAppException extends Exception {
-
-    public PagodaAppException(PagodaAppError error) {
-        super(error.getErrMsg());
-    }
-
-}
